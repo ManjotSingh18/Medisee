4b93c174c681c632548a7a7eb60a82c0cb71a6ec
Manjot <manjos2@uci.edu>
2023-08-16 15:56:55 -0700

    GUI updated

--- gui.py
+++ gui.py
@@ -20,12 +20,12 @@ class mainapp():
     def searcher(self):
         self.icon=ImageTk.PhotoImage(Image.open("./img/logo.PNG"))
         searchmessage=tk.Label(self.frm0, image=self.icon, height=150)
-        searchbox=tk.Text(self.frm0,height="1", font=("Helvetica", 20))
+        searchbox=tk.Text(self.frm0,height="1", font=("Helvetica", 20), bg="lightgrey")
         searchbox.tag_configure("center", justify='center')
-        searchbutt=tk.Button(self.frm0,text="Search", width=20, height=3,justify="center", command=lambda: self.drawdata(self.frm1))
-        nextbutt=tk.Button(self.frm0,text="Next", justify="center",width=20, height=3, command=lambda: self.next(self.frm1))
-        prevbutt=tk.Button(self.frm0,text="Previous", justify="center",width=20, height=3,command=lambda: self.prev(self.frm1))
-        self.pns=pagesearch=tk.Entry(self.frm0, width=25)
+        searchbutt=tk.Button(self.frm0,text="Search", width=20, height=3,justify="center", command=lambda: self.drawdata(self.frm1),bg="lightgrey")
+        nextbutt=tk.Button(self.frm0,text="Next", justify="center",width=20, height=3, command=lambda: self.next(self.frm1),bg="lightgrey")
+        prevbutt=tk.Button(self.frm0,text="Previous", justify="center",width=20, height=3,command=lambda: self.prev(self.frm1), bg="lightgrey")
+        self.pns=pagesearch=tk.Entry(self.frm0, width=25, bg="lightgrey")
         self.pns.insert(0, f"Search page number")
         pagesearch.bind("<FocusIn>", self.temp_text)
         pagesearch.bind("<FocusOut>", self.temp_text2)
@@ -45,8 +45,8 @@ class mainapp():
             self.offset = int(self.pns.get())-1
             show=self.move[self.offset]
             for n in range(0, len(show)):
-                test=tk.Label(self.frm1, text=f"Age: {show[n].age}, Sex: {show[n].sex}, Drugs: {', '.join(show[n].drugs)}\n Reactions: {', '.join(show[n].reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700)
-                test.pack()
+                test=tk.Label(self.frm1, text=f"Age: {show[n].age}, Sex: {show[n].sex}, Drugs: {', '.join(show[n].drugs)}\n\n Reactions: {', '.join(show[n].reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700, bg="#358797")
+                test.pack(pady=10)
             page=tk.Label(self.frm1, text=f"Page {self.offset+1}", font=('Helvetica', 15, 'bold'))
             page.pack()
     def temp_text(self, e):
@@ -58,26 +58,26 @@ class mainapp():
         self.data= home.apicall(self.frm0.winfo_children()[1].get("1.0","end-1c"))
         self.move.clear()
         for i in range(0, len(self.data)):
-            self.move[i//5].append(self.data[i])
+            self.move[i//3].append(self.data[i])
         self.delete(frames)
         x=0
         for case in self.data:
-            if x == 5:
+            if x == 3:
                 break
             else:
-                test=tk.Label(frames, text=f"Age: {case.age}, Sex: {case.sex}, Drugs: {', '.join(case.drugs)}\n Reactions: {', '.join(case.reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700)
-                test.pack()
+                test=tk.Label(frames, text=f"Age: {case.age}, Sex: {case.sex}, Drugs: {', '.join(case.drugs)}\n\n Reactions: {', '.join(case.reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700, bg="#358797")
+                test.pack(pady=10)
             x+=1
         page=tk.Label(frames, text=f"Page 1", font=('Helvetica', 15, 'bold'))
         page.pack()
     def next(self, frames):
-        if self.offset < len(self.data)//5:
+        if self.offset < len(self.data)//3:
             self.delete(frames)
             self.offset+=1
             show=self.move[self.offset]
             for n in range(0, len(show)):
-                test=tk.Label(frames, text=f"Age: {show[n].age}, Sex: {show[n].sex}, Drugs: {', '.join(show[n].drugs)}\n Reactions: {', '.join(show[n].reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700)
-                test.pack()
+                test=tk.Label(frames, text=f"Age: {show[n].age}, Sex: {show[n].sex}, Drugs: {', '.join(show[n].drugs)}\n\n Reactions: {', '.join(show[n].reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700, bg="#358797")
+                test.pack(pady=10)
             page=tk.Label(frames, text=f"Page {self.offset+1}", font=('Helvetica', 15, 'bold'))
             page.pack()
         else:
@@ -90,8 +90,8 @@ class mainapp():
             self.offset-=1
             show=self.move[self.offset]
             for n in range(0, len(show)):
-                test=tk.Label(frames, text=f"Age: {show[n].age}, Sex: {show[n].sex}, Drugs: {', '.join(show[n].drugs)}\n Reactions: {', '.join(show[n].reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700)
-                test.pack()
+                test=tk.Label(frames, text=f"Age: {show[n].age}, Sex: {show[n].sex}, Drugs: {', '.join(show[n].drugs)}\n\n Reactions: {', '.join(show[n].reactions)}\n\n", font=('Helvetica', 10, 'bold'), justify='center', wraplength=700, bg="#358797")
+                test.pack(pady=10)
             page=tk.Label(frames, text=f"Page {self.offset+1}", font=('Helvetica', 15, 'bold'))
             page.pack()
         else:
